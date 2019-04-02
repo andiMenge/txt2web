a792c281d4cb5e8c6f565d2d9aaedd6fc055341e
Andreas Menge <andi.menge@gmail.com>
2019-04-02 18:33:15 +0200

    basic file server

---
+++ txt2web.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"flag"
+)
+
+func main() {
+
+	var port, file string
+
+	// Configure flags
+	flag.StringVar(&port, "port", "8080", "port for the webserver")
+	flag.StringVar(&file, "file", "textfile", "name of the textfile to serve")
+	
+	flag.Parse()
+
+	// root handler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		http.ServeFile(w, r, file)
+	})
+	
+	// Simple static webserver:
+	log.Println("Txt2Web")
+	log.Println("Server running on port:", port)
+	log.Fatal(http.ListenAndServe(":"+ port, nil))
+}
